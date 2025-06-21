4641e914dbe3d24aec4d3038e948b45e834fc85e
ChakNikolay87 <ChakNikolay@Yandex.ru>
2025-06-21 10:47:54 +0300

    feat: Выполнил задание 9-gateway-microservices

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/dto/store/QuantityState.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/dto/store/QuantityState.java
@@ -7,18 +7,5 @@ public enum QuantityState {
     ENDED,
     FEW,
     ENOUGH,
-    MANY;
-
-
-    public static QuantityState fromQuantity(long quantity) {
-        if (quantity <= 0) {
-            return ENDED;
-        } else if (quantity < 10) {
-            return FEW;
-        } else if (quantity <= 100) {
-            return ENOUGH;
-        } else {
-            return MANY;
-        }
-    }
+    MANY
 }
